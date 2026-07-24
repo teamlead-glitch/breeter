2b3a99a518263a6c8dfca00a329188d15cde806e
Sherin Reynold <teamlead@phitany.com>
2026-07-24 18:59:22 +0530

    font conistancy

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next'
-import { Playfair_Display, Inter, DM_Mono } from 'next/font/google'
+import { Inter } from 'next/font/google'
 import './globals.css'
 import Navbar from '@/components/common/navbar/Navbar'
 import Footer from '@/components/common/Footer'
@@ -7,22 +7,9 @@ import WhatsAppFab from '@/components/common/WhatsAppFab'
 import { BookModalProvider } from '@/components/common/BookModalContext'
 import { SearchProvider } from '@/context/SearchContext'
 
-const playfair = Playfair_Display({
-  subsets: ['latin'],
-  variable: '--font-playfair',
-  display: 'swap',
-})
-
 const inter = Inter({
   subsets: ['latin'],
   variable: '--font-inter',
-  display: 'swap',
-})
-
-const dmMono = DM_Mono({
-  subsets: ['latin'],
-  variable: '--font-dm-mono',
-  weight: ['400', '500'],
   display: 'swap',
 })
 
@@ -36,7 +23,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html
       lang="en"
-      className={`${playfair.variable} ${inter.variable} ${dmMono.variable}`}
+      className={inter.variable}
     >
       <body className="min-h-screen flex flex-col">
         <SearchProvider>
